fbfdc74f9197e0807b61c4dc7c30a460db13146a
padovese <bhspadoveze@gmail.com>
2019-07-27 11:08:12 -0300

    initial commit

---
+++ src/main/java/com/padovese/springandreact/SpringandreactApplication.java
@@ -0,0 +1,13 @@
+package com.padovese.springandreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringandreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringandreactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/padovese/springandreact/SpringandreactApplicationTests.java
@@ -0,0 +1,16 @@
+package com.padovese.springandreact;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringandreactApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
