936b31b86070f11de0fe6682773946efe178a115
root <root@home>
2019-07-28 15:16:29 -0300

    setting up versioning in backend to not update same data together

--- src/main/java/com/padovese/springandreact/Employee.java
+++ src/main/java/com/padovese/springandreact/Employee.java
@@ -3,6 +3,9 @@ package com.padovese.springandreact;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Version;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -14,6 +17,8 @@ public class Employee {
 	private String firstName;
 	private String lastName;
 	private String description;
+	
+	private @Version @JsonIgnore Long version;
 	
 	public Employee(){}
 
